c07680db355e3bd272a5f3a54368733a30d1dcf0
Wafi <muhammadcakcop22@gmail.com>
2026-08-31 10:59:36 +0700

    benerin navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -35,7 +35,7 @@
                     Beranda
                 </a>
 
-                @hasanyrole(['Kepala Seksi', 'Kepala Bidang', 'Kepala Sub-Bagian', 'Kepala TU', 'Kepala Kantor'])
+                @hasanyrole(['Kepala Seksi','Kepala Bagian', 'Kepala Bidang', 'Kepala Sub-Bagian', 'Kepala TU', 'Kepala Kantor'])
                     <a href="{{ route('kepala.approval.index') }}" 
                        class="px-5 py-2 rounded-full text-sm transition-all duration-300 flex items-center {{ request()->routeIs('kepala.approval.*') ? 'bg-[#e5edff] font-bold text-[#2A65F3]' : 'font-semibold text-gray-600 hover:text-[#2A65F3] hover:bg-[#e5edff]/60' }}">
                        Approval Cuti
@@ -130,11 +130,6 @@
              <x-responsive-nav-link :href="route('notifikasi')" :active="request()->routeIs('notifikasi')">
                 Notifikasi
             </x-responsive-nav-link>
-        </div>
-        <x-responsive-nav-link :href="route('notifikasi')" :active="request()->routeIs('notifikasi')">
-                Notifikasi
-            </x-responsive-nav-link>
-
             <!-- TAMBAHKAN DUA MENU INI UNTUK VERSI MOBILE -->
             <x-responsive-nav-link :href="route('profile.show')" :active="request()->routeIs('profile.*')">
                 Profil
@@ -149,7 +144,5 @@
                 </x-responsive-nav-link>
             </form>
         </div>
-    </div>
-</nav>
     </div>
 </nav>
